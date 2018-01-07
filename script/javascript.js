9bd1923fa851c4fd1a716782e4d0afbdb8ffff88
MateuszPospiech <pospiechmateusz@gmail.com>
2018-01-07 11:02:48 +0100

    added serInterval

--- script/javascript.js
+++ script/javascript.js
@@ -25,11 +25,11 @@ c.stroke(); // DRAWING
 //Arc / Circle
 
 //Random Color
+
 for (i = 0; i < 100; i++) {
     var redColor = Math.floor(Math.random() * 255); //random "red" color from 0-255 floored
     var greenColor = Math.floor(Math.random() * 255);
     var blueColor = Math.floor(Math.random() * 255);
-
     //Random location
     var x = Math.random() * window.innerWidth; //random x include window width
     var y = Math.random() * window.innerHeight; //random x include window height
@@ -40,3 +40,28 @@ for (i = 0; i < 100; i++) {
     c.strokeStyle = 'rgba(' + redColor + ', ' + greenColor + ', ' + blueColor + ', 1)'; //Stroke style random color
     c.stroke(); //Drawing
 }
+
+//Random color Animation
+
+function animate() {
+    c.clearRect(0, 0, innerWidth, innerHeight) //clear fron point 0, 0 to window Width and Height.
+
+    for (i = 0; i < 100; i++) {
+        var redColor = Math.floor(Math.random() * 255); //random "red" color from 0-255 floored
+        var greenColor = Math.floor(Math.random() * 255);
+        var blueColor = Math.floor(Math.random() * 255);
+        //Random location
+        var x = Math.random() * window.innerWidth; //random x include window width
+        var y = Math.random() * window.innerHeight; //random x include window height
+
+
+        c.beginPath(); //begin to draw
+        c.arc(x, y, 30, 0, Math.PI * 2, false); // X, Y, radius, begin draw engle, end draw engle,
+        c.strokeStyle = 'rgba(' + redColor + ', ' + greenColor + ', ' + blueColor + ', 1)'; //Stroke style random color
+        c.stroke(); //Drawing
+    }
+
+
+};
+
+setInterval(animate, 3000);
